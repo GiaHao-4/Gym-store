2ddd2c597a62926b340a9e8313a0f12e754c1453
Thientran181 <Quangthien181@gmail.com>
2025-12-15 00:52:20 +0700

    addplan

--- Gymmanager/index.py
+++ Gymmanager/index.py
@@ -6,7 +6,7 @@ app = Flask(__name__)
 @app.route('/')
 
 def index():
-    return render_template('hellogym.html')
+    return render_template('addplan.html')
 
 if __name__ == '__main__':
     with app.app_context():
