ed48d285c4a50307bca33043c57efc5e815c8612
GiaHao-4 <2351050040hao@ou.edu.vn>
2025-12-18 21:57:32 +0700

    trang qlhv

--- GiaHao/gym/dao.py
+++ GiaHao/gym/dao.py
@@ -1,14 +1,14 @@
 import json
 import hashlib
-from models import User
+from models import Staff
 from gym import app
 
-def auth_user(username, password):
+def auth_user(username, password, role):
     password = hashlib.md5(password.encode("utf-8")).hexdigest()
-    return User.query.filter(User.username.__eq__(username), User.password.__eq__(password)).first()
+    return Staff.query.filter(Staff.username.__eq__(username), Staff.password.__eq__(password),Staff.role.__eq__(role)).first()
 
 def get_user_by_id(user_id):
-    return User.query.get(user_id)
+    return Staff.query.get(user_id)
 
 if __name__ == "__main__":
     with app.app_context():
--- GiaHao/gym/models.py
+++ GiaHao/gym/models.py
@@ -13,13 +13,26 @@ class UserRole(RoleEnum):
     PT=3
     THUNGAN=4
 
-class User(db.Model, UserMixin):
+class Staff(db.Model, UserMixin):
     user_id = db.Column(db.Integer, primary_key=True, autoincrement=True)
-    username = db.Column(db.String(50), nullable=False)
+    username = db.Column(db.String(50), nullable=False, unique=True)
     password = db.Column(db.String(50), nullable=False)
     full_name = db.Column(db.String(500), nullable=False)
-    email = db.Column(db.String(500), nullable=False)
-    phone = db.Column(db.Integer, nullable=False)
+    email = db.Column(db.String(500), nullable=False, unique=True)
+    phone = db.Column(db.Integer, nullable=False, unique=True)
+    role=db.Column(Enum(UserRole))
+    def get_id(self):
+        return (self.user_id)
+    def __str__(self):
+        return self.full_name
+
+class Member(db.Model, UserMixin):
+    user_id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    username = db.Column(db.String(50), nullable=False, unique=True)
+    password = db.Column(db.String(50), nullable=False)
+    full_name = db.Column(db.String(500), nullable=False)
+    email = db.Column(db.String(500), nullable=False, unique=True)
+    phone = db.Column(db.Integer, nullable=False, unique=True)
     def get_id(self):
         return (self.user_id)
     def __str__(self):
@@ -31,6 +44,9 @@ if __name__ == '__main__':
         import hashlib
 
         password = hashlib.md5("123".encode("utf-8")).hexdigest()
-        u1 = User(username="letan", password=password, full_name="Nguyen Van A", email="nva@gmail.com", phone="12345")
-        db.session.add(u1)
+        u1 = Staff(username="letan", password=password, full_name="Nguyen Van A", email="nva@gmail.com", phone="12345", role=UserRole.LETAN)
+        u2 = Staff(username="test", password=password, full_name="Nguyen Van B", email="nvb@gmail.com", phone="24680", role=UserRole.ADMIN)
+        u3 = Staff(username="test2", password=password, full_name="Nguoi Dung", email="nd@gmail.com", phone="01273", role=UserRole.PT)
+        u4 = Member(username="test3", password=password, full_name="Hoi Vien", email="hv@gmail.com", phone="0849")
+        db.session.add_all([u1, u2, u3, u4])
         db.session.commit()
